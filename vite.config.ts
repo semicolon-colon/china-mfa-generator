8e1b8271c5b1f9532a36a60fdfed16179df52603
semicolon-colon <semicolon.colon20@gmail.com>
2025-11-19 20:20:16 +0900

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
+  base: command === "build" ? "/china-mfa-generator/" : "/",
   plugins: [svelte()],
-})
+}));
